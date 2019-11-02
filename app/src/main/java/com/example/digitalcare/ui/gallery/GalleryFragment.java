92bbc0303f34126355e4a7119cab89ad01a99d42
alucard-alexander <38307899+alucard-alexander@users.noreply.github.com>
2019-11-03 00:42:51 +0530

    now

--- app/src/main/java/com/example/digitalcare/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/digitalcare/ui/gallery/GalleryFragment.java
@@ -1,17 +1,22 @@
 package com.example.digitalcare.ui.gallery;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.digitalcare.Main2Activity;
 import com.example.digitalcare.R;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -22,6 +27,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class GalleryFragment extends Fragment implements OnMapReadyCallback{
 
     private GalleryViewModel galleryViewModel;
+    GoogleMap map123;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -40,10 +46,34 @@ public class GalleryFragment extends Fragment implements OnMapReadyCallback{
         mapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(GoogleMap mMap) {
+                //Toast.makeText(getActivity(), "Ok", Toast.LENGTH_SHORT).show();
+                map123 = mMap;
                 mMap.addMarker(new MarkerOptions().position(new LatLng(13.0305, 77.5649)).title("Marker"));
+                if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    mMap.setMyLocationEnabled(true);
+                } else {
+                    // Show rationale and request permission.
+                    requestPermissions( new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                            9000);
+                }
             }
         });
         return root;
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 9000) {
+            if (permissions.length > 0 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                map123.setMyLocationEnabled(true);
+            } else {
+                // Permission was denied. Display an error message.
+                Toast.makeText(getActivity(), "Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     @Override
